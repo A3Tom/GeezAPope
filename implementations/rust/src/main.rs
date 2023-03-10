a1c6de33963cdcb436aa116139fb18116cbcd0be
A3Tom <Tom_Esq@Yahoo.com>
2023-03-10 19:39:18 +0000

    We now have a random pope, popin up oota the rusty undergrowth. Classssss man

--- implementations/rust/src/main.rs
+++ implementations/rust/src/main.rs
@@ -2,10 +2,10 @@ use std::error::Error;
 use std::io::BufReader;
 use std::path::Path;
 use std::fs;
-
-use models::CenturyPerson;
+use rand::Rng;
 
 mod models;
+use models::CenturyPerson;
 
 fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<CenturyPerson>, Box<dyn Error>> {
     let file = fs::File::open(path).expect("Nae file mate! Get it sorted");
@@ -15,7 +15,11 @@ fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<CenturyPerson>, Bo
 }
 
 fn main() {
-    let file_path = "..\\..\\resources\\popes.json";
+    let file_path = "resources\\popes.json";
     let pope_list = read_user_from_file(&file_path).unwrap();
-    println!("{:?}'", pope_list);
+
+    let mut rng = rand::thread_rng();
+    let pontiff_number = rng.gen_range(1..pope_list.len());
+
+    println!("{:?}", pope_list[pontiff_number]);
 }
